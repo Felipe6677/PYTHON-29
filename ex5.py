00154c0e331eed3d4fc7e919e35a9486ee9ad404
Felipe6677 <felipeprates@colegiopaulodetarso.com.br>
2025-10-29 16:15:23 +0000

    salvo

---
+++ ex5.py
@@ -0,0 +1,13 @@
+ usuarios = ["sembala", "guigo","zard","밤하늘", "" ]
+
+for usuario in usuarios:
+    if not usuario:
+        continue
+        
+        for letra in usuario 
+        if letra.isspace():
+              print(f"ignorando (usuario), (contem espaço vazio)")  
+
+
+    else:
+    print(f"cadastrando usuario (usuario)")    
